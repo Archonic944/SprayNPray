c68ff7f742c1eb3377728abe4826b69b39f83e4c
Archonic <55026152+Archonic944@users.noreply.github.com>
2025-06-29 15:37:57 -0400

    Ink runs out

--- graffitiheroes/MainScene.cs
+++ graffitiheroes/MainScene.cs
@@ -15,6 +15,7 @@ public partial class MainScene : Node2D
 	private RichTextLabel _text;
 	private Area2D _canvas;
 	private Area2D _mouseArea; //is always at the mouse position
+	private TextureRect _sprayCan;
 	private double _lastTextUpdate; // Added for text update timing
 
 	private Random random = new();
@@ -22,6 +23,14 @@ public partial class MainScene : Node2D
 	private HashSet<string> unusedCurves;
 
 	public static readonly int[] StarThresholds = { 540, 625, 680, 740, 800 };
+
+	private void tweenSprayCan()
+	{
+		ShaderMaterial mat = (ShaderMaterial) _sprayCan.GetMaterial();
+		Tween t = CreateTween();
+		t.TweenProperty(mat, "shader_parameter/water_level_percentage", inkLeft / (float)inkStartedWith, 0.2f).FromCurrent();
+		t.TweenCallback(Callable.From(tweenSprayCan));
+	}
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -32,7 +41,12 @@ public partial class MainScene : Node2D
 		_text = GetNode<RichTextLabel>("Control/RichTextLabel");
 		_canvas = GetNode<Area2D>("canvas/Area2D");
 		_mouseArea = GetNode<Area2D>("hitbox");
+		_sprayCan = GetNode<TextureRect>("Spray_Can");
+		Area2D overlapArea = GetNode<Area2D>("Path2D/PathFollow2D/Sprite2D/OverlapArea");
+		overlapArea.AreaEntered += (area) => _currentOverlaps.Add(area);
+		overlapArea.AreaExited += (area) => _currentOverlaps.Remove(area);
 		init();
+		tweenSprayCan();
 		//get all curve resource names
 		curves = ResourceLoader.ListDirectory("res://curves/");
 		unusedCurves = new HashSet<string>(curves);
@@ -71,17 +85,20 @@ public partial class MainScene : Node2D
 	private string state;
 	private List<Sprite2D> _drawnSprites = [];
 	private List<Sprite2D> _sprites = [];
-	private static double _drawSpeed = 0.5f;
+	private static int _drawSpeed = 1000; // pixels per second
 	private static double _finishWaitTime = 1; // seconds to wait after drawing
 	private double _finishTimer;
+	private int inkLeft = 1;
+	private int inkStartedWith = 1;
+	private HashSet<Area2D> _currentOverlaps = [];
+	
 
 	private float _judgingProgress = 0; //progresratio amount
 
 	private static readonly float _possiblePoints = 1000;
 	private float _earnedPoints = 0;
 
-	private float _judgeTime = 2; //seconds
-	private float _judgePathStep = 1f/_possiblePoints;
+	private float _judgeSpeed = 500f; // pixels per second during judging
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
@@ -93,28 +110,19 @@ public partial class MainScene : Node2D
 		_elapsed += delta;
 		if (state == "drawing")
 		{
-			Console.WriteLine("drawing");
-			if (_elapsed - _lastUpdated > DrawRate)
-			{
-				Console.WriteLine("Drawing new sprite");
-				var duplicate = (Sprite2D)_sprite.Duplicate();
-				duplicate.GlobalPosition = _follower.GlobalPosition;
-				AddChild(duplicate);
-				_lastUpdated = _elapsed;
-				_sprites.Add(duplicate);
-			}
+			float curveLength = _path.Curve.GetBakedLength();
+			if (_follower.Progress + _drawSpeed * delta < curveLength) 
+				_follower.Progress += (float)(_drawSpeed * delta);
 			else
 			{
-				//debug
-				Console.WriteLine("Not drawing new sprite, elapsed: " + _elapsed + ", last updated: " + _lastUpdated + ", delta: " + delta);
-			}
-			if (_follower.ProgressRatio + _drawSpeed * delta < 1) _follower.ProgressRatio += (float) _drawSpeed * (float)delta;
-			else
-			{
-				_follower.ProgressRatio = 0;
+				_follower.Progress = 1;
 				state = "drawing_finished";
-				_finishTimer = 0;
 			}
+			//drop a sprite there
+			Sprite2D dot = (Sprite2D) _sprite.Duplicate();
+			AddChild(dot);
+			dot.GlobalPosition = _follower.GlobalPosition;
+			_sprites.Add(dot);
 		}
 		else if (state == "drawing_finished")
 		{
@@ -125,37 +133,50 @@ public partial class MainScene : Node2D
 			// Wait for player to press a button instead of using a timer
 			if (Input.IsActionJustPressed("ui_accept"))
 			{
-				_text.Hide(); // Hide the instruction text
+				_text.Text = "Mouse to draw, SPACE when finished.";
 				
 				Tween t = CreateTween();
 				t.SetParallel();
 				foreach (Sprite2D sprite in _sprites)
 				{
-					sprite.Hide();
 					t.TweenProperty(sprite, "modulate", Colors.Transparent, 1.0f);
 				}
-
-				// Transition directly to player state
+				//calculate how much ink the player needs based on the curve length
+				float curveLength = _path.Curve.GetBakedLength();
+				float inkNeeded = curveLength / 12; // 10 pixels per ink unit
+				inkLeft = (int) Math.Ceiling(inkNeeded);
+				inkStartedWith = (int) Math.Ceiling(inkNeeded);
+				foreach (Sprite2D sprite in _sprites)
+				{
+					sprite.GetNode<Area2D>("OverlapArea").QueueFree();
+				}
+				_currentOverlaps.Clear();
 				state = "player";
 			}
 		}
 		else if (state == "player")
 		{
+			if (inkLeft <= 0) GetNode<Node2D>("Spray_Can/NoMoreInk").Show();
 			_sprite.GlobalPosition = GetGlobalMousePosition();
 			if (_elapsed - _lastUpdated > DrawRate && Input.IsMouseButtonPressed(MouseButton.Left))
 			{
-				if (_canvas.GetOverlappingAreas().Count >= 1)
+				if (_canvas.GetOverlappingAreas().Count >= 1 && _currentOverlaps.Count == 0)
 				{
-					var dot = (Sprite2D)_sprite.Duplicate();
-					dot.GlobalPosition = GetGlobalMousePosition();
-					AddChild(dot);
-					_drawnSprites.Add(dot);
-					_lastUpdated = _elapsed;
+					if (inkLeft > 0)
+					{
+						var dot = (Sprite2D)_sprite.Duplicate();
+						dot.GlobalPosition = GetGlobalMousePosition();
+						AddChild(dot);
+						_drawnSprites.Add(dot);
+						_lastUpdated = _elapsed;
+						inkLeft--;
+					}
 				}
 			}
 
 			if (Input.IsActionJustPressed("ui_accept"))
 			{
+				GetNode<Node2D>("Spray_Can/NoMoreInk").Hide();
 				//begin judging
 				state = "judging";
 				_lastUpdated = _elapsed; 
@@ -169,7 +190,12 @@ public partial class MainScene : Node2D
 			}
 		}else if (state == "judging")
 		{
-			ushort steps = (ushort)(delta / (_judgeTime / _possiblePoints)); //how many full steps we get to do
+			float curveLength = _path.Curve.GetBakedLength();
+			float judgeStepSize = curveLength / _possiblePoints;
+			float judgePixelsPerFrame = _judgeSpeed * (float)delta; // pixels per frame
+			int steps = (int)(judgePixelsPerFrame / judgeStepSize); // How many pixel steps to take this frame
+			if (steps < 1) steps = 1; // Always take at least one step
+			
 			for (int i = 0; i < steps; i++)
 			{
 				//optimistically find distance to closest point
@@ -194,9 +220,11 @@ public partial class MainScene : Node2D
 				line.AddPoint(minVec);
 				line.SetName("_judgeLine " + _elapsed + " #"+ i);
 				_earnedPoints += float.Min(earnedPoint, 1);
-				if (_follower.ProgressRatio + _judgePathStep < 1) _follower.ProgressRatio += _judgePathStep;
+				
+				if (_follower.Progress + judgeStepSize < curveLength) 
+					_follower.Progress += judgeStepSize; // Move 1 pixel at a time
 				else {
-					_follower.ProgressRatio = 1;
+					_follower.Progress = curveLength;
 					break;
 				}
 				Console.WriteLine("Score is now " + _earnedPoints);
@@ -209,7 +237,7 @@ public partial class MainScene : Node2D
 				_lastTextUpdate = _elapsed;
 			}
 
-			if (_follower.ProgressRatio + _judgePathStep > 1)
+			if (_follower.Progress >= curveLength)
 			{
 				state = "judging_finished";
 				Console.Write("Finished judging in " + (_elapsed - _lastUpdated) + "s. Score is "  + _earnedPoints);
@@ -236,7 +264,7 @@ public partial class MainScene : Node2D
 
 	void UpdateText(float score)
 	{
-		_text.Text = "Score: [color=green]" + score + "[/color]\n";
+		_text.Text = "Score: [color=green]" + (int) Math.Round(score) + "[/color]\n";
 		
 		int i = 0;
 		for(; i<StarThresholds.Length; i++)
@@ -272,11 +300,15 @@ public partial class MainScene : Node2D
 				child.QueueFree();
 			}
 		}
+
+		inkLeft = 1;
+		inkStartedWith = 1;
+		_currentOverlaps.Clear();
 		_sprites.Clear();
 		_sprite.Position = Vector2.Zero;
 		_drawnSprites.Clear();
 		_sprite.Show();
-		_follower.ProgressRatio = 0;
+		_follower.Progress = 0;
 		_lastUpdated = 0f;
 		_elapsed = 0;
 		state = "drawing";
